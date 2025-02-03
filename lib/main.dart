9c9bd9b1f339200b17fd64a7b17ca1b337ab1fe2
ahmed adel <ahmedadel123422@gmail.com>
2025-02-03 20:46:31 +0200

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,5 @@
 // // ignore_for_file: prefer_const_constructors
 
-// import 'package:flutter/material.dart';
-// import 'package:hive/hive.dart';
-// import 'package:hive_flutter/hive_flutter.dart';
-// import 'package:muslim_todp/core/services/app_local_storage.dart';
-// import 'package:muslim_todp/pages/home_page.dart';
-// import 'package:muslim_todp/widgets/bottom_nav_bar.dart';
-
-// void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Hive.initFlutter();
-//   await Hive.openBox('user');
-//   AppLocalStorage.init();
-//   runApp(const MainApp());
-// }
-
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       home: NavBarWidget(),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:muslim_todp/core/services/app_local_storage.dart';
